b7f316e1003ffa5ba22eb987302a7e194cbdd0cd
asaleem772 <shahzada.ali.saleem@gmail.com>
2018-07-05 16:56:05 +0100

    Cards Exercise - Go tutorial

--- CARDS/src/main/main.go
+++ CARDS/src/main/main.go
@@ -1,9 +1,19 @@
 package main
 
-import (
-	"fmt"
-)
+//var deck int
 
+//card := "Ace of Spades"
 func main() {
-	fmt.Println("All Set")
+
+	cards := newDeck()
+	//	cards.print()
+	hand, remainingCards := deal(cards, 5)
+
+	hand.print()
+
+	remainingCards.print()
+}
+
+func getCard() string {
+	return "four of Diamonds"
 }
